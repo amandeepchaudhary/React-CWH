4d9dde70771f560bf38658eed8544c76ef4af79e
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-12-11 22:53:56 +0530

    knowing the Props == Properties

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
-let name = "Aman22";
+// let name = "Aman22";
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-      <div className="container">
-        <h1>Hello {name}</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam rem ipsa vero? Dolores commodi alias, aspernatur cumque libero exercitationem est iste facilis maiores sit ut eaque dolorum iusto dolorem voluptatum ducimus rerum mollitia placeat!
-        </p>
-      </div>
-      <div className="blank">Beautiful</div>
-    </nav>
+      {/* <Navbar title="TextUtiles" aboutText="About Us"/> */}
+      {/* <Navbar/> */}
+      <Navbar title="TextUtiles"/>
     </>
   );
 }
---
+++ myapp/src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Navbar(props) {
+    return (
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">{props.title}</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">{props.aboutText}</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <input type="search" className="form-control me-2" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success me-2" type="submit" >Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+//this is used to know about what type we will give to our property
+//isrequired is used to get the value as it is required for our components 
+Navbar.prototype = {
+    title: PropTypes.string.isRequired,
+    aboutText: PropTypes.string.isRequired
+}
+
+//this is used to get a default value in the property if the values in the property is not given, the property must not be "Undefined"
+
+Navbar.defaultProps = {
+    title: "Type the Title Here!!",
+    aboutText: "About"
+}                    
