fa49ba56f5626da141e206e9013bbb0ec3761182
amandeep <amanchaudhary.ac1@gmail.com>
2023-01-15 04:49:11 +0530

    used use state for dark mode and added About

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import { useState } from 'react';
+
 // let name = "Aman22";
 function App() {
+  const [mode, setMode] = useState("light");  //whether dark mode is enables or not
   return (
     <>
       {/* <Navbar title="TextUtiles" aboutText="About Us"/> */}
       {/* <Navbar/> */}
-      <Navbar title="TextUtiles"/>
+      <Navbar title="TextUtiles" mode={mode}/>
       <div className="container my-3">
         <TextForm heading="Enter your text here"/>
+        <About/>
       </div>
     </>
   );
