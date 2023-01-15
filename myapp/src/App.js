3df76addf76123eeaf2486c15b9ebcdf62679eef
amandeep <amanchaudhary.ac1@gmail.com>
2023-01-15 16:50:08 +0530

    added a arrow function and usestate for textform dark mode

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,14 +7,18 @@ import { useState } from 'react';
 // let name = "Aman22";
 function App() {
   const [mode, setMode] = useState("light");  //whether dark mode is enables or not
+  const [dma, setDma] = useState("white")
+
   const dm = ()=>{
     if(mode==="light"){
       setMode("dark");
       document.body.style.backgroundColor = 'black';
+      setDma("black");
     }
     else{
       setMode("light")
       document.body.style.backgroundColor = 'white';
+      setDma("white");
     }
   }
   return (
@@ -24,7 +28,7 @@ function App() {
       <Navbar title="TextUtiles" mode={mode} enable={dm}/>
       <div className="container my-3">
         <TextForm heading="Enter your text here"/>
-        <About/>
+        <About enable={dm} col={dma}/>
       </div>
     </>
   );
